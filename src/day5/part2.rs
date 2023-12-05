a8034c84f299dc8c711f24c4e836047ccc51e96e
Micah Weitzman <mikachoow21@gmail.com>
2023-12-05 13:17:02 -0800

    Completed day5

--- src/day5/part2.rs
+++ src/day5/part2.rs
@@ -2,14 +2,84 @@
 use crate::utils::read_lines;
 
 pub fn main(filename: &str) {
-  read_lines(filename)
-                      .iter()
-                      .for_each(|l| println!("{}", l));
-                      // .map(|line| prase_line(line))
-                      // .sum();
-  // println!("{}", res);
-}
+  let lines = read_lines(filename);
 
-fn prase_line(line: &String) -> u32 {
-  0
+  let mut it = lines.iter().peekable();
+
+  let mut seeds_vec: Vec<(i64, i64)> = vec![];
+
+  let mut seeds_soil: Vec<(i64, i64, i64)> = vec![];
+  let mut soil_fert: Vec<(i64, i64, i64)> = vec![];
+  let mut fert_water: Vec<(i64, i64, i64)> = vec![];
+  let mut water_light: Vec<(i64, i64, i64)> = vec![];
+  let mut light_temp: Vec<(i64, i64, i64)> = vec![];
+  let mut temp_hum: Vec<(i64, i64, i64)> = vec![];
+  let mut hum_loc: Vec<(i64, i64, i64)> = vec![];
+
+
+  while it.peek().is_some() {
+    let line = it.next().unwrap();
+
+    if line.starts_with("seeds: ") {
+      seeds_vec = line.split(": ")
+                      .last()
+                      .unwrap()
+                      .split(" ")
+                      .map(|n| n.parse::<i64>().unwrap())
+                      .collect::<Vec<i64>>()
+                      .chunks(2)
+                      .map(|x| (x[0], x[1]))
+                      .collect();
+      seeds_vec.sort_by_key(|k| k.0);
+    }
+    if line.ends_with("map:") {
+      let vec = match line.split(" ").next().unwrap() {
+        "seed-to-soil" => &mut seeds_soil,
+        "soil-to-fertilizer" => &mut soil_fert,
+        "fertilizer-to-water" => &mut fert_water,
+        "water-to-light" => &mut water_light,
+        "light-to-temperature" => &mut light_temp,
+        "temperature-to-humidity" => &mut temp_hum,
+        "humidity-to-location" => &mut hum_loc,
+        _ => panic!("map not found")
+      };
+
+      while it.peek().is_some() && !it.peek().unwrap().is_empty() {
+        let nums: Vec<i64> = it.next()
+                               .unwrap()
+                               .split(" ")
+                               .map(|n| n.parse::<i64>().unwrap())
+                               .collect();
+        let start = nums[1];
+        let stop = start + nums[2] - 1;
+        let inc = nums[0] - nums[1];
+        vec.push((start, stop, inc));
+      }
+      vec.sort_by_key(|k| k.0);
+    }
+  }
+
+  // This is not performant, but totally works. ~40min
+  let res: i64 = seeds_vec.into_iter()
+    .map(|(strt_rng, end_rng)|
+      (strt_rng..strt_rng+end_rng).map(|seed|
+        [&seeds_soil,&soil_fert,&fert_water,&water_light,&light_temp,&temp_hum,&hum_loc]
+          .iter()
+          .fold(seed, |acc, x| {
+            for (start, end, inc) in *x {
+              if acc >= *start && acc <= *end {
+                return acc + inc;
+              }
+            }
+            acc
+          })
+      )
+      .min()
+      .unwrap()
+  )
+  .min()
+  .unwrap();
+  println!("{}", res);
+
+  // Answer: 60294664
 }
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,26 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+// mod day6;
+// mod day7;
+// mod day8;
+// mod day9;
+// mod day10;
+// mod day11;
+// mod day12;
+// mod day13;
+// mod day14;
+// mod day15;
+// mod day16;
+// mod day17;
+// mod day18;
+// mod day19;
+// mod day20;
+// mod day21;
+// mod day22;
+// mod day23;
+// mod day24;
+// mod day25;
 
 mod utils;
 
@@ -23,13 +43,9 @@ fn main() {
     _ => "input.txt"
   };
 
-  let new_dir = Path::new("./src").join(day);
-
-  env::set_current_dir(new_dir);
-
-
-  // utils::read_lines("test.txt").iter().for_each(|l| println!("{}", l));
-
+  env::set_current_dir(
+    Path::new("./src").join(day)
+  );
 
   match (day, part) {
     ("day1", "part1") => day1::part1::main(filename),
